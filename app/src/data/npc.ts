d481332493ad46d65d62f46e97242720054b6a90
Oskar Świda <oskar.swida@gmail.com>
2022-08-29 23:33:49 +0200

    pdf print

---
+++ app/src/data/npc.ts
@@ -0,0 +1,374 @@
+const npcSurnameTable = [
+  "Diseth",
+  "Avison",
+  "Sonoda",
+  "Dilucca",
+  "Rocheford",
+  "Sabine",
+  "Cayne",
+  "Esperon",
+  "Cedeno",
+  "Forgrave",
+  "Basset",
+  "Belmont",
+  "Lockley",
+  "Blackford",
+  "Flanigan",
+  "Kendrick",
+  "Lane",
+  "Dial",
+  "Khalifa",
+  "Black",
+  "Ashley",
+  "Kenner",
+  "Blue",
+  "Sims",
+  "Kent",
+  "Dkymm",
+  "Gunner",
+  "Fox",
+  "Carter",
+  "Silver",
+  "Rhoades",
+  "Ivy",
+  "Malkova",
+  "Reid",
+  "White",
+  "Vox",
+  "Fawx",
+  "Grey",
+  "Griffith",
+  "Jensen",
+  "Rain",
+  "Sunderland",
+  "Ames",
+  "Lynn",
+];
+
+const npcFemaleNameTable = [
+    1
+
+Toni
+
+2
+
+Simula
+
+3
+
+Akiko
+
+4
+
+Parity
+
+5
+
+Gerty
+
+6
+
+Shaylea
+
+7
+
+Kalle
+
+8
+
+Star
+
+9
+
+Ayvee
+
+10
+
+Chang
+
+11
+
+Alexa
+
+12
+
+Adi
+
+13
+
+Lexie
+
+14
+
+Angel
+
+15
+
+Ning
+
+16
+
+Sprite
+
+17
+
+Io
+
+18
+
+Anna
+
+19
+
+Lunar
+
+20
+
+Alita
+
+21
+
+Asta
+
+22
+
+Pipa
+
+23
+
+Pixel
+
+24
+
+Link
+
+25
+
+Amiga
+
+26
+
+Meta
+
+27
+
+Organia
+
+28
+
+Chyu
+
+29
+
+Tiff
+
+30
+
+Gai
+
+31
+
+Jeri
+
+32
+
+Rina
+
+33
+
+Tetra
+
+34
+
+Iris
+
+35
+
+Nyx
+
+36
+
+Poly
+
+37
+
+Electra
+
+38
+
+Ayaka
+
+39
+
+Mae
+
+40
+
+Nion
+
+41
+
+Astrid
+
+42
+
+Aria
+
+43
+
+Orlaith
+
+44
+
+Annora
+
+45
+
+Vanina
+
+46
+
+Kaitlyn
+
+47
+
+Nikki
+
+48
+
+Anna
+
+49
+
+Kira
+
+50
+
+Molly
+
+51
+
+Kacey
+
+52
+
+Carli
+
+53
+
+Zazie
+
+54
+
+Kayley
+
+55
+
+Aidra
+
+56
+
+Lenna
+
+57
+
+Liya
+
+58
+
+Lana
+
+59
+
+Madyson
+
+60
+
+Demi
+
+61
+
+Val
+
+62
+
+Kendra
+
+63
+
+Skylar
+
+64
+
+Xev
+
+65
+
+Britt
+
+66
+
+Leah
+
+67
+
+Alina
+
+68
+
+Petra
+
+69
+
+Caprice
+
+70
+
+Megan
+
+71
+
+Sasha
+
+72
+
+Karla
+
+73
+
+Romi
+
+74
+
+Tasha
+
+75
+
+Edyn
+
+76
+
+Brandi
+
+77
+
+Nora
+
+78
+
+Nova
+
+79
+
+Daiko
+
+80
+
+Dani
+
+81
+
+Kitana
+];
+
+export { npcSurnameTable };
